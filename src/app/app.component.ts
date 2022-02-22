ecd5936a001f69cad0fc3571c287288641f13fda
Karthik <karthik.kaliyaperumal@innoart.in>
2022-02-22 10:58:53 +0530

    new design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular Quiz app';
+  title = 'Angular Quiz New Design';
 }

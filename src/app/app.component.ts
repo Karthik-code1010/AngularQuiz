88112bd5d95b3912421045d4b1cdf51373831d8d
Karthik <karthik.kaliyaperumal@innoart.in>
2022-03-02 10:02:11 +0530

    change and hithub package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular Quiz App1111';
+  title = 'Angular Quiz App11112222';
 }

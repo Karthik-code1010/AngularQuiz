02aa3d8927624bbba51298d7fa438f86b8dbf8b0
Karthik <karthik.kaliyaperumal@innoart.in>
2022-03-09 14:03:25 +0530

    change title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular Quiz App444444';
+  title = 'Angular Quiz App4444445555';
 }
